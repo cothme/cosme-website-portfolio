0944546515302d02ae40942b1e6b41c0831abd89
cothme <miguicosme.hs@gmail.com>
2024-09-20 18:55:15 +0800

    Build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
-import { Hero } from "./components/Hero";
 import MainBody from "./components/MainBody";
 
 function App() {
